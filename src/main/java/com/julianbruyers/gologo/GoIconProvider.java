fa5c4a328580d279fa8bc8532adabaff1a493ae6
Julian Bruyers <julian.bruyers@gmail.com>
2025-10-15 14:38:17 +0200

    feat: Rescale "Go" icon to fit in with the other icons in the file explorer

--- src/main/java/com/julianbruyers/gologo/GoIconProvider.java
+++ src/main/java/com/julianbruyers/gologo/GoIconProvider.java
@@ -2,14 +2,14 @@ package com.julianbruyers.gologo;
 
 import com.intellij.ide.IconProvider;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.util.Iconable;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.util.Iconable;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.util.IconUtil;
+import javax.swing.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
 
 /**
  * Icon provider for PSI elements (Project Structure Interface).
@@ -17,17 +17,41 @@ import javax.swing.*;
  */
 public class GoIconProvider extends IconProvider implements DumbAware {
 
-    private static final Icon GO_FILE_ICON = IconLoader.getIcon("/icons/go_file.svg", GoIconProvider.class.getClassLoader());
-    private static final Icon GO_MOD_ICON = IconLoader.getIcon("/icons/go_mod.svg", GoIconProvider.class.getClassLoader());
-    private static final Icon GO_SUM_ICON = IconLoader.getIcon("/icons/go_sum.svg", GoIconProvider.class.getClassLoader());
+    private static final Icon GO_FILE_ICON = IconUtil.scale(
+        IconLoader.getIcon(
+            "/icons/go_file.svg",
+            GoIconProvider.class.getClassLoader()
+        ),
+        null,
+        1.95f
+    );
+    private static final Icon GO_MOD_ICON = IconUtil.scale(
+        IconLoader.getIcon(
+            "/icons/go_mod.svg",
+            GoIconProvider.class.getClassLoader()
+        ),
+        null,
+        1.95f
+    );
+    private static final Icon GO_SUM_ICON = IconUtil.scale(
+        IconLoader.getIcon(
+            "/icons/go_sum.svg",
+            GoIconProvider.class.getClassLoader()
+        ),
+        null,
+        1.95f
+    );
 
     @Nullable
     @Override
-    public Icon getIcon(@NotNull PsiElement element, @Iconable.IconFlags int flags) {
+    public Icon getIcon(
+        @NotNull PsiElement element,
+        @Iconable.IconFlags int flags
+    ) {
         if (element instanceof PsiFile) {
             PsiFile psiFile = (PsiFile) element;
             String fileName = psiFile.getName().toLowerCase();
-            
+
             if (fileName.equals("go.mod")) {
                 return GO_MOD_ICON;
             } else if (fileName.equals("go.sum")) {
@@ -36,7 +60,7 @@ public class GoIconProvider extends IconProvider implements DumbAware {
                 return GO_FILE_ICON;
             }
         }
-        
+
         return null;
     }
 }
--- src/main/java/com/julianbruyers/gologo/GoLogo.java
+++ src/main/java/com/julianbruyers/gologo/GoLogo.java
@@ -5,10 +5,10 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.IconUtil;
+import javax.swing.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
 
 /**
  * File icon provider for Go-related files.
@@ -16,25 +16,41 @@ import javax.swing.*;
  */
 public class GoLogo implements FileIconProvider, DumbAware {
 
-    private static final Icon GO_FILE_ICON = IconLoader.getIcon("/icons/go_file.svg", GoLogo.class.getClassLoader());
-    private static final Icon GO_MOD_ICON = IconLoader.getIcon("/icons/go_mod.svg", GoLogo.class.getClassLoader());
-    private static final Icon GO_SUM_ICON = IconLoader.getIcon("/icons/go_sum.svg", GoLogo.class.getClassLoader());
+    private static final Icon GO_FILE_ICON = IconUtil.scale(
+        IconLoader.getIcon("/icons/go_file.svg", GoLogo.class.getClassLoader()),
+        null,
+        1.95f
+    );
+    private static final Icon GO_MOD_ICON = IconUtil.scale(
+        IconLoader.getIcon("/icons/go_mod.svg", GoLogo.class.getClassLoader()),
+        null,
+        1.95f
+    );
+    private static final Icon GO_SUM_ICON = IconUtil.scale(
+        IconLoader.getIcon("/icons/go_sum.svg", GoLogo.class.getClassLoader()),
+        null,
+        1.95f
+    );
 
     @Nullable
     @Override
-    public Icon getIcon(@NotNull VirtualFile file, int flags, @Nullable Project project) {
+    public Icon getIcon(
+        @NotNull VirtualFile file,
+        int flags,
+        @Nullable Project project
+    ) {
         if (file == null) return null;
-        
+
         String fileName = file.getName().toLowerCase();
-        
+
         // Handle go.mod files
         if (fileName.equals("go.mod")) {
             return GO_MOD_ICON;
-        } 
-        // Handle go.sum files  
+        }
+        // Handle go.sum files
         else if (fileName.equals("go.sum")) {
             return GO_SUM_ICON;
-        } 
+        }
         // Handle .go files
         else if (fileName.endsWith(".go")) {
             return GO_FILE_ICON;
